bc80e252a25e61bb691ab89be21846a94b486c63
abirichi <tripathyabhijeet72@yahoo.com>
2021-07-01 23:43:45 +0530

    Modifying feature commit

--- src/com/abhi/intellij/install/hello/TestInstall.java
+++ src/com/abhi/intellij/install/hello/TestInstall.java
@@ -5,5 +5,6 @@ public class TestInstall {
     public static void main(String[] args){
         System.out.println("Hello SS, Intellij");
         System.out.println("Updating line for feature commit");
+        System.out.println("Added New feature commit line");
     }
 }
