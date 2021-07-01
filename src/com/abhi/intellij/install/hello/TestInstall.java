314d0a331d021c28ee8f9a553c12cb9af71ff8ef
abirichi <tripathyabhijeet72@yahoo.com>
2021-07-01 23:38:35 +0530

    Feature Pull Request (#1)
    Merge pull request #1
    Added Feature Changes

--- src/com/abhi/intellij/install/hello/TestInstall.java
+++ src/com/abhi/intellij/install/hello/TestInstall.java
@@ -4,5 +4,6 @@ public class TestInstall {
 
     public static void main(String[] args){
         System.out.println("Hello SS, Intellij");
+        System.out.println("Updating line for feature commit");
     }
 }
